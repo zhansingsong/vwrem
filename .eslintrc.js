c100a1f5ad715b3b9cc077dd923a0ec7d97ec0d0
apple <apple@AppledeMacBook-Pro.local>
2019-04-15 16:57:28 +0800

    build: add README.md and enable eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2013
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+	'env': {
+		'browser': true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 3
+	},
+	'rules': {
+		'indent': [
+			'error',
+			2
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
 };
--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-!function(win, MAX_WIDTH, MIN_WIDTH){
+!(function(win, division, MAX_WIDTH, MIN_WIDTH) {
+  division = division || 10;
+  MAX_WIDTH = MAX_WIDTH || 1200;
+  MIN_WIDTH = MIN_WIDTH || 320;
   // make sure that checkVUnit is only called once.
-  var checkVwUnit = function () {
+  var checkVwUnit = function() {
     var foolElement = document.createElement('div');
     foolElement.style.width = '10vw';
     var docElement = document.documentElement;
     docElement.appendChild(foolElement);
-    var compStyle = parseInt((win.getComputedStyle ?
-                          getComputedStyle(foolElement, null) :
-                          foolElement.currentStyle).width, 10);
+    var compStyle = parseInt(
+      (win.getComputedStyle
+        ? getComputedStyle(foolElement, null)
+        : foolElement.currentStyle
+      ).width,
+      10
+    );
     win.__is_vw__ = parseInt(win.innerWidth / 10, 10) === compStyle;
     docElement.removeChild(foolElement);
-  }
+  };
   checkVwUnit();
 
   var fontSize;
   var fitCB = function() {
     var innerWidth = win.innerWidth;
-    if(innerWidth > MAX_WIDTH){
-      fontSize = MAX_WIDTH / 10 + 'px';
+    if (innerWidth > MAX_WIDTH) {
+      fontSize = MAX_WIDTH / division + 'px';
     } else if (innerWidth < MIN_WIDTH) {
-      fontSize = MIN_WIDTH / 10 + 'px';
+      fontSize = MIN_WIDTH / division + 'px';
     } else {
-      fontSize = win.__is_vw__ ? '10vw' : innerWidth / 10 + 'px';
+      fontSize = win.__is_vw__ ? '10vw' : innerWidth / division + 'px';
     }
     document.documentElement.style.fontSize = fontSize;
-  }
+  };
   var throttle = function(fn, delay) {
-    let timer;
-    return function(){
-      if(timer){
-      clearTimeout(timer);
+    var timer;
+    return function() {
+      if (timer) {
+        clearTimeout(timer);
         timer = null;
       }
-      timer = setTimeout(fn, delay)
-    }
-  }
+      timer = setTimeout(fn, delay);
+    };
+  };
   fitCB();
   win.addEventListener('resize', throttle(fitCB, 100), false);
-  win.addEventListener('pageshow', function (event) {
+  win.addEventListener('pageshow', function(event) {
     if (event.persisted) {
-      fitCB()
+      fitCB();
     }
   });
-}(window, 1600, 320);
+})(window);
